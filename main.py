948f3688511b2c91e55a857e8c1b6383a223a119
FerrumCyan <112402244+FerrumCyan@users.noreply.github.com>
2023-01-10 14:59:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html",img_list=img)
+    return render_template("index.html")
+
+@app.route("/admin2")
+def index():
+    return render_template("admin2.html")
     
 app.run()
