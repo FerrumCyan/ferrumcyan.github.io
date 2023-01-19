cbe88d4955083553a42b8c45a0b349e4cdfe9cc5
FerrumCyan <112402244+FerrumCyan@users.noreply.github.com>
2023-01-20 07:55:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import *
-from getmusic import *
-from getimg import *
-from getmovie import *
 app = Flask(__name__)
 #主页
 @app.route("/")
+def index():
+    return render_template("/index.html")
+
+@app.route("/index")
 def index():
     return render_template("/index.html")
 #VIP{
 @app.route("/a")
 def index():
     return render_template("/VIP/魏在宸/index.html")
-    
+}
 app.run()
