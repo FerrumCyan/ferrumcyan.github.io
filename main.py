8798415978559770a3be0b7ada2ca4e637e54905
FerrumCyan <112402244+FerrumCyan@users.noreply.github.com>
2023-01-28 10:20:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ app = Flask(__name__)
 #主页
 @app.route("/")
 def index():
-    return render_template("/index.html")
+    return render_template("index.html")
 
-@app.route("/index")
+@app.route("/框架")
 def index():
-    return render_template("/index.html")
+    return render_template("Basic.html")
 #VIP{
-@app.route("/比特股")
+@app.route("/a")
 def index():
-    return render_template("/VIP/魏在宸/index.html")
+    return render_template("indexvip1.html")
 }
 app.run()
