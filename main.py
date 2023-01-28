ad1cab2860e43d1344429981183110609544ad4e
FerrumCyan <112402244+FerrumCyan@users.noreply.github.com>
2023-01-28 10:27:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ app = Flask(__name__)
 #主页
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("/index.html")
 
 @app.route("/框架")
 def index():
-    return render_template("Basic.html")
+    return render_template("/Basic.html")
 #VIP{
 @app.route("/a")
 def index():
-    return render_template("indexvip1.html")
-}
+    return render_template("/VIP/魏在宸/index.html")
+#}
 app.run()
