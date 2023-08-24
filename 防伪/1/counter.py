71bd35a9da54aef24d6a28a3260000e108a93f53
FerrumCyan <112402244+FerrumCyan@users.noreply.github.com>
2023-08-24 10:01:31 +0800

    Update counter.py

--- 防伪/1/counter.py
+++ 防伪/1/counter.py
@@ -1,6 +1,7 @@
-# counter.py
+from flask import Flask
 
-# Read the current count from the file
+app = Flask(__name__)
+
 def read_count():
     try:
         with open('count.txt', 'r') as file:
@@ -8,13 +9,17 @@ def read_count():
     except FileNotFoundError:
         return 0
 
-# Increment and save the count
 def increment_count():
     count = read_count()
     count += 1
     with open('count.txt', 'w') as file:
         file.write(str(count))
+    return count
 
-# Main program
+@app.route('/count')
+def get_count():
+    count = increment_count()
+    return str(count)
+
 if __name__ == '__main__':
-    increment_count()
+    app.run(host='0.0.0.0', port=5000)
