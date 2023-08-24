7a3845121385be255a792fee80e7ea731a02960a
FerrumCyan <112402244+FerrumCyan@users.noreply.github.com>
2023-08-24 09:43:05 +0800

    Update count.js

--- 防伪/1/count.js
+++ 防伪/1/count.js
@@ -1,22 +1,29 @@
 // 获取当前访问次数
 function getCount() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/get_count", false);
+    xhr.open("GET", "/get_count", true); // 设置异步为true
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            document.getElementById("count").textContent = xhr.responseText;
+        }
+    };
     xhr.send();
-    return parseInt(xhr.responseText);
 }
 
 // 更新访问次数
 function updateCount() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/update_count", false);
+    xhr.open("GET", "/update_count", true); // 设置异步为true
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            getCount(); // 更新访问次数显示
+        }
+    };
     xhr.send();
-    return parseInt(xhr.responseText);
 }
 
 // 页面加载完成时更新访问次数
 window.onload = function () {
-    var count = getCount();
-    document.getElementById("count").textContent = count;
+    getCount(); // 获取并显示当前访问次数
     updateCount(); // 更新访问次数
 };
