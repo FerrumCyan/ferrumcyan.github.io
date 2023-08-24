f1640ee75b447adb69346ed4272de62bc9fa30e8
FerrumCyan <112402244+FerrumCyan@users.noreply.github.com>
2023-08-24 09:40:22 +0800

    Update count.py

--- 防伪/1/count.py
+++ 防伪/1/count.py
@@ -1,30 +1,40 @@
-#!/usr/bin/env python3
-import cgi
+import http.server
+import socketserver
 import os
 
 count_file = "visit_count.txt"  # 存储访问次数的文件名
 
-def get_count():
-    try:
-        with open(count_file, "r") as file:
-            count = file.read()
-            return int(count) if count.strip().isdigit() else 0
-    except FileNotFoundError:
-        return 0
+class MyHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path.startswith("/get_count"):
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(str(self.get_count()).encode())
+        elif self.path.startswith("/update_count"):
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(str(self.update_count()).encode())
+        else:
+            super().do_GET()
 
-def update_count():
-    count = get_count() + 1
-    with open(count_file, "w") as file:
-        file.write(str(count))
-    return count
+    def get_count(self):
+        try:
+            with open(count_file, "r") as file:
+                count = file.read()
+                return int(count) if count.strip().isdigit() else 0
+        except FileNotFoundError:
+            return 0
 
-form = cgi.FieldStorage()
-action = form.getvalue("action", "")
+    def update_count(self):
+        count = self.get_count() + 1
+        with open(count_file, "w") as file:
+            file.write(str(count))
+        return count
 
-print("Content-type: text/plain\n")
+PORT = 8000
 
-if action == "get":
-    print(get_count())
-elif action == "update":
-    new_count = update_count()
-    print(new_count)
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("Serving at port", PORT)
+    httpd.serve_forever()
